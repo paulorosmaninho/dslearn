b1bfc38964bd83719331bd877904fc6dd9b3384b
Paulo Rosmaninho <paulo.rosmaninho@gmail.com>
2022-02-21 16:03:38 -0300

    UserController

--- backend/src/main/java/com/devsuperior/dslearnbds/controllers/UserController.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/controllers/UserController.java
@@ -1,5 +1,77 @@
 package com.devsuperior.dslearnbds.controllers;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.devsuperior.dslearnbds.dtos.UserDTO;
+import com.devsuperior.dslearnbds.dtos.UserInsertDTO;
+import com.devsuperior.dslearnbds.dtos.UserUpdateDTO;
+import com.devsuperior.dslearnbds.services.UserService;
+
+@RestController
+@RequestMapping(value = "/users")
 public class UserController {
+
+	@Autowired
+	private UserService userService;
+
+	@GetMapping
+	public ResponseEntity<Page<UserDTO>> findAll(Pageable pageable) {
+
+		Page<UserDTO> pageUserDTO = userService.findAllPaged(pageable);
+
+		return ResponseEntity.ok().body(pageUserDTO);
+
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable Long id) {
+
+		UserDTO userDTO = userService.findById(id);
+
+		return ResponseEntity.ok().body(userDTO);
+	}
+
+	@PostMapping
+	public ResponseEntity<UserDTO> insert(@Valid @RequestBody UserInsertDTO userInsertDTO) {
+
+		UserDTO userDTO = userService.insert(userInsertDTO);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(userDTO.getId())
+				.toUri();
+
+		return ResponseEntity.created(uri).body(userDTO);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> update(@PathVariable Long id, @Valid @RequestBody UserUpdateDTO userUpdateDTO) {
+
+		UserDTO userDTO = userService.update(id, userUpdateDTO);
+
+		return ResponseEntity.ok().body(userDTO);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+		userService.delete(id);
+
+		return ResponseEntity.noContent().build();
+	}
 
 }
